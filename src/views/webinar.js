dac85d64c5c4b0fbdccc9c04967c04934c7d407b
pankowskit <dagi@10g.pl>
2022-08-23 00:40:39 +0200

    layout

--- src/views/webinar.js
+++ src/views/webinar.js
@@ -1,10 +1,19 @@
-import {Container} from "react-bootstrap";
+import React,{Component} from "react";
+import Container from "../../node_modules/react-bootstrap/Container";
+import WebinarS1 from "../components/webinar-s1";
+import WebinarS2 from "../components/webinar-s2";
+import WebinarS3 from "../components/webinar-s3";
 
-function Webinar(){
-    return(
-        <Container fluid className="minh-footer-adj bg-dark d-flex align-items-center py-5">
-            
-        </Container>
-    );
+class Webinar extends Component{
+    render(){
+        return(        
+            <Container fluid className="minh-footer-adj p-0">
+                <WebinarS1/>
+                <WebinarS2/>
+                <WebinarS3/>
+            </Container>    
+        );
+    }
 }
+
 export default Webinar;
